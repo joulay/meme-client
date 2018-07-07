e24735ebd0d45df88996db88e1a625f5f49d6e5f
Julie K <julieskim160@gmail.com>
2018-07-07 00:38:10 -0700

    position without absolute

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,46 @@ import {Route, withRouter} from 'react-router-dom';
 
 import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
+import RegistrationPage from './registration-page';
+import {refreshAuthToken} from '../actions/auth';
 import './App.css';
 
 class App extends Component {
-  render() {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+        // When we are logged in, refresh the auth token periodically
+        this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+        // Stop refreshing when we log out
+        this.stopPeriodicRefresh();
+    }
+}
+
+componentWillUnmount() {
+    this.stopPeriodicRefresh();
+}
+
+startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+        () => this.props.dispatch(refreshAuthToken()),
+        60 * 60 * 1000 // One hour
+    );
+}
+
+stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+        return;
+    }
+
+    clearInterval(this.refreshInterval);
+}
+
+render() {
     return (
       <div className="App">
           <HeaderBar />
-          <Route exact path="/" component={LandingPage} />        
+          <Route exact path="/" component={LandingPage} />     
+          <Route exact path="/register" component={RegistrationPage} />   
       </div>
     );
   }
